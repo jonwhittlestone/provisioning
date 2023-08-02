9a584c96ff560e3ec9eff0a1fa785d775be67b55
Jon Whittlestone <dev@howapped.com>
2023-08-02 10:52:17 +0100

    Rename

--- nixos-config/modules/system.nix
+++ nixos-config/modules/system.nix
@@ -79,7 +79,7 @@
   };
 
   fonts = {
-    fonts = with pkgs; [
+    packages = with pkgs; [
       # icon fonts
       material-design-icons
 
@@ -93,7 +93,7 @@
     ];
 
     # use fonts specified by user rather than default ones
-    enableDefaultFonts = false;
+    enableDefaultPackages = false;
 
     # user defined fonts
     # the reason there's Noto Color Emoji everywhere is to override DejaVu's
